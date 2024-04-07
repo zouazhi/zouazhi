8437e5a95caade0f8978c02929f28bd6efd8c2d2
zouazhi <91052711+zouazhi@users.noreply.github.com>
2024-04-07 18:56:42 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,14 +13,21 @@ sudo ufw enable   #开启ufw
 
 mkdir ppp ; cd ppp 
 
-带uring的要求内核大于5.10，内存256，其他用兼容版本，aarch为arm CPU
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64-io-uring.zip && unzip openppp2-linux-amd64-io-uring.zip && rm openppp2-linux-amd64-io-uring.zip
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64.zip && unzip openppp2-linux-amd64.zip && rm openppp2-linux-amd64.zip
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64-io-uring.zip && unzip openppp2-linux-aarch64-io-uring.zip && rm openppp2-linux-aarch64-io-uring.zip
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64.zip && unzip openppp2-linux-aarch64.zip && rm openppp2-linux-aarch64.zip
+#带uring的要求内核大于5.10，内存256，其他用兼容版本，aarch为arm CPU
+#amd64
+ #内核≥5.10 内存≥256MB
+wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64-io-uring.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip' -exec rm {} +
+ #内核≤5.10
+wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip' -exec rm {} +
+
+#arm-aarch
+ #内核≥5.10 内存≥256MB
+wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64-io-uring.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip' -exec rm {} +
+ #内核≤5.10
+wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip' -exec rm {} +
 
 
-自己配置的文件
+#配置文件
 wget -O appsettings.json https://raw.githubusercontent.com/zouazhi/zouazhi/main/appsettings.json
 
 ##修改配置文件  2222→ip 当然端口也可以改 20000→端口
