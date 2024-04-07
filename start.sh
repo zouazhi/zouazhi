d0152312cb5028e13ae95250f06e8162ff1e9b21
zouazhi <91052711+zouazhi@users.noreply.github.com>
2024-04-07 19:01:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,15 +16,15 @@ mkdir ppp ; cd ppp
 #带uring的要求内核大于5.10，内存256，其他用兼容版本，aarch为arm CPU
 #amd64
  #内核≥5.10 内存≥256MB
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64-io-uring.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip' -exec rm {} +
+wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64-io-uring.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip*' -exec rm {} +
  #内核≤5.10
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip' -exec rm {} +
+wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64.zip && unzip openppp2*.zip* && find . -type f -name '*openppp2*.zip*' -exec rm {} +
 
 #arm-aarch
  #内核≥5.10 内存≥256MB
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64-io-uring.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip' -exec rm {} +
+wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64-io-uring.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip*' -exec rm {} +
  #内核≤5.10
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip' -exec rm {} +
+wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64.zip && unzip openppp2*.zip && find . -type f -name '*openppp2*.zip*' -exec rm {} +
 
 
 #配置文件
