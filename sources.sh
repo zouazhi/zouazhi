f1d303625a7a293c06052f37e8b06501c91e7cba
zouazhi <91052711+zouazhi@users.noreply.github.com>
2025-03-29 09:54:49 +0800

    Update sources.sh

--- sources.sh
+++ sources.sh
@@ -4,6 +4,10 @@
 sudo sed -i 's/^#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  1000/' /etc/gai.conf
 echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4 > /dev/null
 
+# 安装 wget 和 curl 工具
+ sudo apt update
+ sudo apt install -y wget curl
+
 # 备份原有的软件源文件
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
