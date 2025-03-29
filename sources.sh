2930c54a6ad11094102bcb470ba92f1069cbae9e
zouazhi <91052711+zouazhi@users.noreply.github.com>
2025-03-29 10:06:02 +0800

    Update sources.sh

--- sources.sh
+++ sources.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# 安装 wget 和 curl 工具
+sudo apt update
+sudo apt install -y wget curl
+
 # 获取主机名
 hostname=$(hostname)
 
 # 获取 IPv4 地址
-ipv4_address=$(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n 1)
+ipv4_address=$(curl -s https://ip.sb)
 
 # 更新 /etc/hosts 文件
 if ! grep -q "$hostname" /etc/hosts; then
@@ -14,10 +18,6 @@ fi
 # 设置网络优先使用 IPv4
 sudo sed -i 's/^#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  1000/' /etc/gai.conf
 echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4 > /dev/null
-
-# 安装 wget 和 curl 工具
-sudo apt update
-sudo apt install -y wget curl
 
 # 备份原有的软件源文件
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
@@ -43,6 +43,10 @@ EOF
 
 # 更新软件包索引
 apt update
+
+# 恢复网络配置
+sudo sed -i 's/^precedence ::ffff:0:0\/96  1000/#precedence ::ffff:0:0\/96  100/' /etc/gai.conf
+sudo rm /etc/apt/apt.conf.d/99force-ipv4
 
 # 删除自身脚本
 rm -- "$0"
