9c477ec0c3b44149b9a542ab5289ff2bcacf9310
zouazhi <91052711+zouazhi@users.noreply.github.com>
2025-06-14 00:27:47 +0800

    Update start.sh

--- ppp/start.sh
+++ ppp/start.sh
@@ -1,43 +1,15 @@
-#检查内核
-uname -a
-or 
-dpkg --list | grep linux-image
-
-安装依赖，前置，防火墙
-apt update && apt install sudo -y 
-sudo apt install screen unzip wget nano -y
-
-配置防火墙
-sudo apt install ufw -y
-sudo ufw allow 22/tcp && sudo ufw allow 20000  #放行22 20000
-sudo ufw enable   #开启ufw
-
 mkdir ppp ; cd ppp 
 
-#带uring的要求内核大于5.10，内存256，其他用兼容版本，aarch为arm CPU
+mkdir /opt/ppp && cd /opt/ppp
 #amd64
- #内核≥5.10 内存≥256MB
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64-io-uring.zip && unzip -o openppp2*.zip 'ppp' && unzip -n openppp2*.zip -x 'ppp' && find . -type f -name '*openppp2*.zip*' -exec rm {} +
  #内核≤5.10
 wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64.zip && unzip -o openppp2*.zip 'ppp' && unzip -n openppp2*.zip -x 'ppp' && find . -type f -name '*openppp2*.zip*' -exec rm {} +
 
-#arm-aarch
- #内核≥5.10 内存≥256MB
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64-io-uring.zip && unzip -o openppp2*.zip 'ppp' && unzip -n openppp2*.zip -x 'ppp' && find . -type f -name '*openppp2*.zip*' -exec rm {} +
- #内核≤5.10
-wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64.zip && unzip -o openppp2*.zip 'ppp' && unzip -n openppp2*.zip -x 'ppp' && find . -type f -name '*openppp2*.zip*' -exec rm {} +
-
-
 #配置文件
-wget -O appsettings.json https://raw.githubusercontent.com/zouazhi/zouazhi/main/appsettings.json
+wget -O appsettings.json https://raw.githubusercontent.com/zouazhi/zouazhi/main/ppp/config/appsettings.json
 
 ##修改配置文件  2222→ip 当然端口也可以改 20000→端口
 nano appsettings.json
 
 screen -S ppp #新建screen ppp
 chmod +x ppp && ./ppp -m -s #PPP~启动！
-
-apt update && apt install sudo -y && sudo apt install screen unzip wget nano -y  && mkdir ppp ; cd ppp && wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-aarch64.zip && unzip -o openppp2*.zip 'ppp' && unzip -n openppp2*.zip -x 'ppp' && find . -type f -name '*openppp2*.zip*' -exec rm {} + && wget -O appsettings.json https://raw.githubusercontent.com/zouazhi/zouazhi/main/appsettings.json && nano appsettings.json && screen -S ppp
-
-
-chmod +x ppp && ./ppp -m -s
