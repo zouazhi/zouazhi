2e0d1eb73e2a6f217e83850ad7457b29b3a6abaa
zouazhi <91052711+zouazhi@users.noreply.github.com>
2025-06-14 00:37:50 +0800

    Update second.sh

--- ppp/second.sh
+++ ppp/second.sh
@@ -14,9 +14,17 @@ wget -O appsettings.json https://raw.githubusercontent.com/zouazhi/zouazhi/main/
 nano appsettings.json
 
 #配置服务
-wget -P /etc/systemd/system https://raw.githubusercontent.com/zouazhi/zouazhi/main/ppp/ppp.service
+wget -P /etc/systemd/system https://raw.githubusercontent.com/zouazhi/zouazhi/main/ppp/config/ppp.service
 
-chmod +x /root/ppp/ && chmod +x /root/ppp/ppp && systemctl daemon-reload && systemctl enable ppp.service  && systemctl start ppp.service && systemctl status ppp.service
+chmod +x /opt/ppp/ && chmod +x /opt/ppp/ppp && systemctl daemon-reload && systemctl enable ppp.service  && systemctl start ppp.service && systemctl status ppp.service
+
+
+
+wget https://github.com/liulilittle/openppp2/releases/latest/download/openppp2-linux-amd64.zip && unzip -o openppp2*.zip 'ppp' && unzip -n openppp2*.zip -x 'ppp' && find . -type f -name '*openppp2*.zip*' -exec rm {} + && wget -O appsettings.json https://raw.githubusercontent.com/zouazhi/zouazhi/main/ppp/config/appsettings.json && nano appsettings.json
+
+
+wget -P /etc/systemd/system https://raw.githubusercontent.com/zouazhi/zouazhi/main/ppp/config/ppp.service && chmod +x /opt/ppp/ && chmod +x /opt/ppp/ppp && systemctl daemon-reload && systemctl enable ppp.service  && systemctl start ppp.service && systemctl status ppp.service
+
 
 systemctl enable ppp.service
 systemctl daemon-reload
